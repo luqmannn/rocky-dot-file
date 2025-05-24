82626428a47ba1075645cba275c58bf918080aa4
luqmannn <m.luqman.hamdan@gmail.com>
2025-05-25 06:36:11 +0800

    Replace echo with printf and add %s

--- script/sysdetail.sh
+++ script/sysdetail.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/bash
 
-echo "$(cat /etc/os-release | head -n2)"
-echo ""
+os_version=$(head -n2 /etc/os-release)
+printf "%s\n" "$os_version"
+printf "\n"
 
-echo "User: $(who | awk '/seat0/ {print $1}')"
-echo "Time: $(who | awk '/seat0/ {print $3, $4}')"
-echo ""
+user=$(who | awk '/seat0/ {print $1}')
+login_time=$(who | awk '/seat0/ {print $3, $4}')
+printf "User: %s\n" "$user"
+printf "Login: %s\n" "$login_time"
+printf "\n"
 
-echo "Kernel: $(uname -r)"
-echo "Root: $(df -h | awk '/rl-root/ {print $3,"/",$2,"-",$5}')"
-echo "Boot: $(df -h | awk '/nvme/ {print $3,"/",$2,"-",$5}')"
-echo "RAM: $(free -h | awk '/^Mem:/ {print $3,"/",$2}')"
-echo "Swap: $(free -h | awk '/^Swap:/ {print $3,"/",$2}')"
+kernel=$(uname -r)
+root_pttn=$(df -h | awk '/rl-root/ {print $3,"/",$2,"-",$5}')
+boot_pttn=$(df -h | awk '/nvme/ {print $3,"/",$2,"-",$5}')
+ram=$(free -h | awk '/^Mem:/ {print $3,"/",$2}')
+swap=$(free -h | awk '/^Swap:/ {print $3,"/",$2}')
+
+printf "Kernel: %s\n" "$kernel"
+printf "Root: %s\n" "$root_pttn"
+printf "Boot: %s\n" "$boot_pttn"
+printf "RAM: %s\n" "$ram"
+printf "Swap: %s\n" "$swap"
